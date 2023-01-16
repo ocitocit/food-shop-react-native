d53cab137b56b0f31cbbe827554301375edd9347
ocitocit <ocritocrit@gmail.com>
2023-01-16 23:01:07 +0700

    adding svg library and add some page

---
+++ src/pages/SplashScreen/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {Logo} from '../../assets';
+
+function SplashScreen() {
+  return (
+    <View>
+      <Logo />
+      <Text>Food</Text>
+    </View>
+  );
+}
+
+export default SplashScreen;
